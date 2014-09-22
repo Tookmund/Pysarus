11527d669d2e1d5b9c5545f0383755b6fc030253
Jacob Adams <Toukmond@users.noreply.github.com>
2014-09-22 10:06:40 -0400

    Create pysarus.py

---
+++ pysarus.py
@@ -0,0 +1,13 @@
+#Pysarus
+#A python wrapper for Big Huge Thesarus
+#Jacob Adams
+
+import urllib.request
+import json
+
+#where to save reteived defintions
+#if None than no files are saved
+savepath = "."
+
+def dict(word):
+	pass
