decb1eee9764d3ee299f268df17d3895a946b6a4
Jacob Adams <Toukmond@users.noreply.github.com>
2014-09-23 14:28:58 -0400

    More work on pysarus
    added hashbang
    renamed main function

--- pysarus.py
+++ pysarus.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 #Pysarus
 #A python wrapper for Big Huge Thesarus
 #Jacob Adams
@@ -12,9 +13,14 @@ savepath = "."
 #Your API key for Big Huge Thesarus
 APIkey = ""
 
-def dict(word):
+def thesarus(word):
 	if(savepath):
 		try:
 			return json.load(savepath+word)
-		else:
-			pass
+		except:
+			print("No file found in "+savepath+" for "+word)
+		web = urllib.request.urlopen("http://words.bighugelabs.com/api/2/"+word+"/json")
+		jsonobj = json.loads(web.read())
+		if(savepath):
+			json.dump(jsonobj,savepath+word)
+		return jsonobj
